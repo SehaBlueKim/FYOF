2eaffd8076e77c6c69586df44c7cf450f4a36461
SehaBlueKim <superhyper.com@gmail.com>
2025-07-03 18:20:26 +0900

    [docs style 디테일 완성]

--- js/sub/community/community.js
+++ js/sub/community/community.js
@@ -37,3 +37,12 @@ view_more_btn.addEventListener("click", function () {
     });
   }, 50);
 });
+
+const goToTop_btn = document.getElementById("goToTop_btn");
+
+goToTop_btn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
---
+++ js/sub/library/detail.js
@@ -0,0 +1,8 @@
+const goToTop_btn = document.getElementById("goToTop_btn");
+
+goToTop_btn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
