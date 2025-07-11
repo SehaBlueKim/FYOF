f3fd8d14658265a3b72fcb68307ff33632d38a1d
SehaBlueKim <superhyper.com@gmail.com>
2025-07-11 18:11:04 +0900

    [docs style 향수 찾기 수정, 디테일 수정]

--- js/sub/library/detail.js
+++ js/sub/library/detail.js
@@ -1,3 +1,4 @@
+// 좋아요
 const heartBtn = document.querySelector(".heartBtn");
 
 heartBtn.addEventListener("click", function () {
@@ -18,5 +19,36 @@ goToTop_btn.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
+  });
+});
+
+// 팔로우 버튼
+const followBtns = document.querySelectorAll(".followBtn");
+
+followBtns.forEach((btn) => {
+  const icon = btn.querySelector(".followIcon");
+  const text = btn.querySelector(".followText");
+
+  let isFollow = false;
+
+  btn.addEventListener("click", function () {
+    isFollow = !isFollow;
+
+    if (isFollow) {
+      btn.style.backgroundColor = "var(--mono-color2)";
+      btn.style.fontSize = "14px";
+      icon.src = "/img/detail/user_check.svg";
+      icon.style.filter =
+        "brightness(0) saturate(100%) invert(4%) sepia(13%) saturate(22%) hue-rotate(328deg) brightness(96%) contrast(88%)";
+      text.style.color = "var(--mono-color4)";
+      text.textContent = "팔로잉";
+    } else {
+      btn.style.backgroundColor = "";
+      icon.src = "/img/detail/user_plus.svg";
+      icon.style.filter =
+        "brightness(0) saturate(100%) invert(91%) sepia(99%) saturate(10%) hue-rotate(249deg) brightness(104%) contrast(96%)";
+      text.style.color = "var(--mono-color1)";
+      text.textContent = "팔로우";
+    }
   });
 });
