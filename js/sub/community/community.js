ad113d607a64e58d22b5e18adbe0811f724a4c54
김세하 <superhyper.com@gmail.com>
2025-09-26 00:19:43 +0900

    [docs style 소개 페이지 작업 중]

--- js/sub/community/community.js
+++ js/sub/community/community.js
@@ -38,6 +38,25 @@ view_more_btn.addEventListener("click", function () {
   }, 50);
 });
 
+// 모바일 gnb
+if (window.matchMedia("(min-width: 320px) and (max-width: 768px)").matches) {
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector(".icons button:last-child");
+    const gnb = document.getElementById("gnb");
+
+    menuBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      gnb.classList.toggle("active");
+    });
+    document.addEventListener("click", (e) => {
+      if (!gnb.contains(e.target) && !menuBtn.contains(e.target)) {
+        gnb.classList.remove("active");
+      }
+    });
+  });
+}
+
+// go to top 버튼
 const goToTop_btn = document.getElementById("goToTop_btn");
 
 goToTop_btn.addEventListener("click", function () {
