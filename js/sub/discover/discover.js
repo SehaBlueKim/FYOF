4a986a2f3dbe9219ae4cd672e80d43ec0271af85
김세하 <superhyper.com@gmail.com>
2025-09-28 22:18:24 +0900

    [docs style 탐색 페이지 모바일 작업 중]

--- js/sub/discover/discover.js
+++ js/sub/discover/discover.js
@@ -73,3 +73,21 @@ checkbox2.addEventListener("change", function () {
     capacityMax.value = "";
   }
 });
+
+// 모바일 gnb
+if (window.matchMedia("(min-width: 320px) and (max-width: 768px)").matches) {
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.querySelector(".icons button:last-child");
+    const gnb = document.getElementById("gnb");
+
+    menuBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      gnb.classList.toggle("active");
+    });
+    document.addEventListener("click", (e) => {
+      if (!gnb.contains(e.target) && !menuBtn.contains(e.target)) {
+        gnb.classList.remove("active");
+      }
+    });
+  });
+}
